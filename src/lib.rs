3b78667e211c683b6eb2833c1d2e347a2e9f9a1b
刘荣飞 <30670835@qq.com>
2021-10-18 18:08:44 +0800

    实现尾插

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@
 mod simple_list;
 
 /// 不使用unsafe实现单链表增删改查
-pub mod safe_list;
+pub mod unsafe_list;
 
--- src/unsafe_list.rs
+++ src/unsafe_list.rs
@@ -1,17 +1,10 @@
-use std::{
-    cell::RefCell,
-    fmt::Debug,
-    marker::{self, PhantomData},
-    mem,
-    ptr::NonNull,
-    rc::Rc,
-};
+use std::{fmt::Debug, marker::PhantomData, mem, ptr::NonNull};
 
 /// 节点，用于保存数据
 #[derive(Debug)]
 pub struct Node<T>
 where
-    T: Debug,
+    T: Ord,
 {
     next: Option<NonNull<Node<T>>>,
 
@@ -21,26 +14,26 @@ where
 
 impl<T> Node<T>
 where
-    T: Debug,
+    T: Ord,
 {
     fn new(data: T) -> Self {
         Node { next: None, data }
     }
 }
 
-impl<T> Drop for Node<T>
+impl<T> Node<T>
 where
-    T: Debug,
+    T: Ord,
 {
-    fn drop(&mut self) {
-        println!("drop:{:?}", self.data)
+    fn into_node(self: Box<Self>) -> T {
+        self.data
     }
 }
 
 /// 链表
 pub struct LinkedList<T>
 where
-    T: Debug,
+    T: Ord,
 {
     // 头指针
     head: Option<NonNull<Node<T>>>,
@@ -52,7 +45,7 @@ where
 
 impl<T> LinkedList<T>
 where
-    T: Debug,
+    T: Ord,
 {
     /// 新建链表
     pub fn new() -> Self {
@@ -75,17 +68,65 @@ where
         self.len += 1;
     }
 
-    pub fn pop(&mut self) -> Option<Node<T>> {
-        unsafe {
-            let v = if let Some(v) = self.head {
-                *Box::from_raw(v.as_ptr())
-            } else {
-                return None;
-            };
-            self.head = v.next;
-            Some(v)
+    pub fn push_back(&mut self, data: T) {
+        // 创建节点，返回指针
+        let node = Some(Box::leak(Box::new(Node::new(data))).into());
+
+        // 链表节点数+1
+        self.len += 1;
+
+        // 如果链表为空，添加为第一个元素
+        if self.len == 0 {
+            self.head = node;
+            return;
+        }
+
+        // 临时指针，用于每次循环
+        let mut p = self.head;
+
+        loop {
+            let mut flag = false;
+
+            let mut cur = p.unwrap().as_ptr();
+            let mut t = unsafe { Box::from_raw(cur) };
+
+            let v = t.next;
+            if v == None {
+                t.next = node;
+                flag = true;
+            }
+            p = v;
+            cur = Box::leak(t.into());
+            if flag {
+                break;
+            }
         }
     }
+
+    /// 从头部获取一个元素
+    pub fn pop(&mut self) -> Option<T> {
+        let v = if let Some(v) = self.head {
+            unsafe { Box::from_raw(v.as_ptr()) }
+        } else {
+            return None;
+        };
+        self.head = v.next;
+        self.len -= 1;
+        Some(v.into_node())
+    }
+
+    /// 从尾部获取一个元素
+    pub fn pop_tail(&mut self) -> Option<T> {
+        // 没节点的情况
+        if self.len == 0 {
+            return None;
+        } else if self.len == 1 {
+        }
+        None
+    }
+
+    /// 逆序链表
+    pub fn rev(&mut self) {}
 
     pub fn iter(&self) -> Iter<'_, T> {
         let v = *self.head.as_ref().unwrap();
@@ -98,18 +139,18 @@ where
 
 impl<T> Drop for LinkedList<T>
 where
-    T: Debug,
+    T: Ord,
 {
     fn drop(&mut self) {
         struct DropGuard<'a, T>(&'a mut LinkedList<T>)
         where
-            T: Debug;
+            T: Ord;
 
         impl<'a, T> Drop for DropGuard<'a, T>
         where
-            T: Debug,
+            T: Ord,
         {
-            // 当下面的循环出现错误的时候，这个drop就会被执行，继续释放所有的元素， 
+            // 当下面的循环出现错误的时候，这个drop就会被执行，继续释放所有的元素，
             // 下面的while循环正常执行的时候，由于是调用forget方法，他不会调用drop函数，这里的drop就不会被调用。
             fn drop(&mut self) {
                 while self.0.pop().is_some() {}
@@ -117,6 +158,7 @@ where
         }
 
         while let Some(node) = self.pop() {
+            // println!("drop: {:?}", &node as *const T);
             let guard = DropGuard(self);
             drop(node);
             mem::forget(guard);
@@ -139,7 +181,7 @@ where
 
 pub struct Iter<'a, T: 'a>
 where
-    T: Debug,
+    T: Ord,
 {
     // 记录当前遍历的节点
     cur: Option<NonNull<Node<T>>>,
@@ -148,7 +190,7 @@ where
 
 impl<'a, T> Iterator for Iter<'a, T>
 where
-    T: Debug,
+    T: Ord,
 {
     type Item = &'a T;
 
@@ -170,56 +212,29 @@ mod tests {
     use super::LinkedList;
 
     #[test]
-    fn list_new() {
-        let mut list = LinkedList::new();
-        list.push("a".to_string());
-        list.push("b".to_string());
-        list.push("c".to_string());
-
-        let mut node = list.head;
-        while let Some(v) = node {
-            unsafe {
-                let d = &*v.as_ptr();
-                println!("{}", d.data);
-                node = d.next;
-            }
-        }
-
-        println!("1 finish");
-
-        let mut node = list.head;
-        while let Some(v) = node {
-            unsafe {
-                let d = &*v.as_ptr();
-                println!("{}", d.data);
-                node = d.next;
-            }
-        }
-
-        println!("2 finish");
-    }
-
-    #[test]
     fn list_iter() {
         let mut list = LinkedList::new();
-        list.push("a".to_string());
-        list.push("b".to_string());
-        list.push("c".to_string());
 
-        for v in list.iter() {
-            println!("{}", v);
-        }
+        list.push("a".to_string());
+        list.push_back("b".to_string());
+        list.push("c".to_string());
+        list.push_back("d".to_string());
+
+        let res: Vec<&String> = list.iter().collect();
+
+        assert_eq!(vec!["c", "a", "b", "d"], res);
     }
 
-    #[test]
-    fn list_pop() {
-        let mut list = LinkedList::new();
-        list.push("a".to_string());
-        list.push("b".to_string());
-        list.push("c".to_string());
+    // #[test]
+    // fn list_pop() {
+    //     let mut list = LinkedList::new();
+    //     list.push(1);
+    //     list.push(2);
+    //     list.push(3);
+    //     list.push(4);
 
-        while let Some(v) = list.pop() {
-            println!("{:?}", v.data);
-        }
-    }
+    //     while let Some(v) = list.pop_tail() {
+    //         println!("{:?}", v);
+    //     }
+    // }
 }
